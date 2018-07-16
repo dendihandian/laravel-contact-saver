b14da54be8679a3a6d4e9def3063aa69312e4c54
Dendi Handian <handian.dendi@gmail.com>
2018-07-16 23:20:51 +0700

    add bg-dark and text-light class to some component

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
   <div class="container">
-    <div class="jumbotron">
+    <div class="jumbotron bg-dark text-light">
       <h1 class="display-4">Save your contact now</h1>
       <p class="lead">Anyone, Anywhere, Anytime.</p>
-      <hr class="my-4">
+      <hr class="my-4" style="background-color: white;">
       <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
       <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
     </div>
