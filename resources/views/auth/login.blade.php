9db9465883f3ce11b2c9c2397ad3a884e515ab3f
Dendi Handian <handian.dendi@gmail.com>
2018-07-16 23:29:32 +0700

    add border class to cards component

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card border border-dark">
                 <div class="card-header bg-dark text-light">{{ __('Login') }}</div>
 
                 <div class="card-body">
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card border border-dark">
                 <div class="card-header text-light bg-dark">{{ __('Register') }}</div>
 
                 <div class="card-body">
