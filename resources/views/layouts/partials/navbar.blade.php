4d888057162f8664c0b32bf2e674643a3f09adee
Dendi Handian <handian.dendi@gmail.com>
2018-07-18 22:41:51 +0700

    contact tab only for authenticated user

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -10,7 +10,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <!-- Left Side Of Navbar -->
             <ul class="navbar-nav mr-auto">
-              <li class="nav-item"><a href="{{ route('contacts.index') }}" class="nav-link">Contacts</a></li>
+              @auth
+                <li class="nav-item"><a href="{{ route('contacts.index') }}" class="nav-link">Contacts</a></li>
+              @endauth
             </ul>
 
             <!-- Right Side Of Navbar -->
