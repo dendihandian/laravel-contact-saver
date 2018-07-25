038312c76bf8052859085a8a58cc543f85c818b8
Dendi Handian <handian.dendi@gmail.com>
2018-07-25 21:30:07 +0700

    update group seeder

--- database/factories/GroupFactory.php
+++ database/factories/GroupFactory.php
@@ -3,8 +3,10 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Group::class, function (Faker $faker) {
+    $name = $faker->word;
     return [
       'owner_id' => function () { return factory(App\User::class)->create()->id; },
       'name' => $faker->word,
+      'slug' => str_slug($name),
     ];
 });
