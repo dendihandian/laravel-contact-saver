81c0abb8edd52460aa5ea74ce6b94301b155232f
Dendi Handian <handian.dendi@gmail.com>
2018-08-09 15:37:20 +0700

    add other group to default

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -6,9 +6,11 @@ class Group extends Model
 {
     const FAMILIES_GROUP_ID = 1;
     const FRIENDS_GROUP_ID = 2;
+    const OTHER_GROUP_ID = 3;
     const DEFAULT_GROUP_ID_ARRAY = [
       self::FAMILIES_GROUP_ID,
       self::FRIENDS_GROUP_ID,
+      self::OTHER_GROUP_ID,
     ];
 
     public function getRouteKeyName()
